23d7a0780feb72762402a1fe35b1a07622211fc2
Laptop <rubin3215@gmail.com>
2025-04-20 15:25:55 +0300

    base structure

---
+++ src/main/java/ru/course/sensor/logger/SensorLoggerApplication.java
@@ -0,0 +1,13 @@
+package ru.course.sensor.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorLoggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/course/sensor/logger/SensorLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.course.sensor.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
